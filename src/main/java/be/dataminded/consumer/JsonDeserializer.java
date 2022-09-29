0c6c316181c718f9916bb5feb006d74cd2afc054
Jelle De Vleminck <jelle.de.vleminck@gmail.com>
2022-09-29 21:51:17 +0200

    Commit pom.xml and serializers + AggregateSensorEvent separately

---
+++ src/main/java/be/dataminded/consumer/JsonDeserializer.java
@@ -0,0 +1,49 @@
+package be.dataminded.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class JsonDeserializer<T> implements Deserializer<T> {
+
+    private final ObjectMapper objectMapper;
+    private Class<T> cls;
+
+    public JsonDeserializer(Class<T> cls) {
+        this.cls = cls;
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Override
+    public void configure(Map configs, boolean isKey) {
+
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try {
+            return this.objectMapper.readValue(data, cls);
+        } catch (IOException e) {
+            throw new SerializationException(e);
+        }
+    }
+
+    @Override
+    public T deserialize(String topic, Headers headers, byte[] data) {
+        return deserialize(topic, data);
+    }
+
+    @Override
+    public void close() {
+    }
+}
---
+++ src/main/java/be/dataminded/producer/JsonSerializer.java
@@ -0,0 +1,41 @@
+package be.dataminded.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class JsonSerializer<T> implements Serializer<T> {
+    private final ObjectMapper objectMapper;
+
+    public JsonSerializer() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Override
+    public void configure(Map<String, ?> config, boolean isKey) {
+        //Nothing to Configure
+    }
+
+    @Override
+    public byte[] serialize(String topic, T data) {
+        if (data == null) {
+            return null;
+        }
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (JsonProcessingException e) {
+            throw new SerializationException("Error serializing JSON message", e);
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
